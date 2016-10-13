c7f094be7bba8bf7d252d8443c4b78e5011d7b42
Hardi Probst <hprobst@it-economics.de>
2016-10-13 23:54:25 +0200

    fixed kibana

--- log-analysis/kibana/install.sh
+++ log-analysis/kibana/install.sh
@@ -1,3 +1,6 @@
-wget -nv https://download.elastic.co/kibana/kibana/kibana-4.6.1-linux-x86_64.tar.gz
-tar xzf kibana-4.6.1-linux-x86_64.tar.gz -C /var/lib/tomcat7/webapps
-rm kibana-4.6.1-linux-x86_64.tar.gz
+KIBANA_BASENAME=kibana-4.6.1-linux-x86_64
+KIBANA_ARCHIVE=$KIBANA_BASENAME'.tar.gz'
+wget -nv https://download.elastic.co/kibana/kibana/$KIBANA_ARCHIVE
+tar xzf $KIBANA_ARCHIVE -C /data
+rm $KIBANA_ARCHIVE
+ln -s /data/$KIBANA_BASENAME /data/kibana
