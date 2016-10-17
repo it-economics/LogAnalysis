fd320bc1741748997e937fa90cb9eb06ffd6920f
Sebastian Bezold <sbezold@it-economics.de>
2016-10-17 14:16:22 +0200

    Create a custom entrypoint and default filebeat config

---
+++ order-service/src/main/docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Starting spring boot app'
+echo 'Outputs are redirected to /var/log/'
+java -jar /app.jar 2> /var/log/app-error.log > /var/log/app-stdout.log
+
+echo 'Starting filebeat'
+/etc/init.d/filebeat start
--- order-service/src/main/docker/install-filebeat.sh
+++ order-service/src/main/docker/install-filebeat.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -L -O https://download.elastic.co/beats/filebeat/filebeat_1.3.1_amd64.deb
 dpkg -i filebeat_1.3.1_amd64.deb
 rm filebeat_1.3.1_amd64.deb
