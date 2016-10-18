0563be790091cb95e59bad67fa1de7a5c7c73529
Sebastian Bezold <sbezold@it-economics.de>
2016-10-18 12:58:38 +0200

    Start filebeat before launching order-service app

--- order-service/src/main/docker/entrypoint.sh
+++ order-service/src/main/docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo 'Starting spring boot app'
-echo 'Outputs are redirected to /var/log/'
-java -jar /app.jar 2> /var/log/app-error.log > /var/log/app-stdout.log
+touch /var/log/app-stdout.log
 
 echo 'Starting filebeat'
 /etc/init.d/filebeat start
+
+echo 'Starting spring boot app'
+echo 'Outputs are redirected to /var/log/'
+java -jar /app.jar 2> /var/log/app-error.log > /var/log/app-stdout.log
