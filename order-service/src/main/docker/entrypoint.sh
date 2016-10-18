15642957da5de6a84f7d1eb5379d9573d1da4efe
Sebastian Bezold <sbezold@it-economics.de>
2016-10-18 14:58:55 +0200

    Stalling order-serivce startup till ES finished

--- order-service/src/main/docker/entrypoint.sh
+++ order-service/src/main/docker/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# create empty logfile to secure proper start of filebeat
 touch /var/log/app-stdout.log
+
+# captured from https://github.com/deviantony/docker-elk/blob/master/kibana/entrypoint.sh ;)
+# Wait for the Elasticsearch container to be ready before interacting with it.
+echo "Stalling for Elasticsearch"
+while true; do
+    nc -q 1 elasticsearch 9200 2>/dev/null && break
+done
+
+#echo "Elasticsearch ready! Publishing filebeat template"
+curl -XPUT 'http://elasticsearch:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
 
 echo 'Starting filebeat'
 /etc/init.d/filebeat start
