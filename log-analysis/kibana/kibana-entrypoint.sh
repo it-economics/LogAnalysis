6ad83e96d2015d67660107858c26afab4280ebc1
Sebastian Bezold <sbezold@it-economics.de>
2016-10-18 12:00:30 +0200

    Add stalling of kibana startup till ES finished starting

--- log-analysis/kibana/kibana-entrypoint.sh
+++ log-analysis/kibana/kibana-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# captured from https://github.com/deviantony/docker-elk/blob/master/kibana/entrypoint.sh ;)
+# Wait for the Elasticsearch container to be ready before starting Kibana.
+echo "Stalling for Elasticsearch"
+while true; do
+    nc -q 1 elasticsearch 9200 2>/dev/null && break
+done
+
 cd beats-dashboards-1.3.1/
 ./load.sh -url elasticsearch:9200
 
