bb494b84ab4c5a926424272c3a7c93c8ffb452d9
roland.voelker@gmx.de <lord_helmchen>
2016-10-20 08:36:52 +0200

    Application.java und pom aufgeräumt; move zu application.properties; controller gebaut

--- order-service/src/main/java/hello/Application.java
+++ order-service/src/main/java/hello/Application.java
@@ -1,56 +1,13 @@
 package hello;
 
-import hello.log.LoggingTaskService;
-import hello.order.OrderPurchaseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@ComponentScan("hello")
 public class Application {
 
-    @Autowired
-    private OrderPurchaseService orderScheduler;
-
-    @Autowired
-    private LoggingTaskService loggingSchedulerService;
-
-    @RequestMapping("/")
-    public String home() {
-        return "Elasticsearch - Simple Logging Application";
-    }
-
-    @RequestMapping("/log/start")
-    public String log() {
-        loggingSchedulerService.scheduleLogTasks();
-        return "Log task running. See standard output";
-    }
-
-    @RequestMapping("/log/stop")
-    public String cancelLog() {
-        loggingSchedulerService.cancelLogTasks();
-        return "Log task stopped.";
-    }
-
-    @RequestMapping("/order/start")
-    public String order() {
-        orderScheduler.scheduleOrderTasks();
-        return "Order logs running. See standard output";
-    }
-
-    @RequestMapping("/order/stop")
-    public String cancelOrder() {
-        orderScheduler.cancelOrderTasks();
-        return "Order logs stopped.";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
