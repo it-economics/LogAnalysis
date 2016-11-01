300fed83c5410e05ca3683c535c7eb6da8dd7bf2
dlorian <fdorau@it-economics.de>
2016-11-01 20:47:50 +0100

    Added logstash-logback-encoder to enable JSON logging

--- order-service/src/main/java/hello/ApplicationConfig.java
+++ order-service/src/main/java/hello/ApplicationConfig.java
@@ -1,9 +1,12 @@
 package hello;
 
+import net.logstash.logback.encoder.LogstashEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class ApplicationConfig {
@@ -12,5 +15,10 @@ public class ApplicationConfig {
     public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         return scheduler;
+    }
+
+    @Bean
+    public LogstashEncoder logstashEncoder() {
+        return new LogstashEncoder();
     }
 }
