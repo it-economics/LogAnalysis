2991730d15367eee9f9dd1c371bc342326f42769
Sebastian Bezold <sbezold@it-economics.de>
2016-11-02 09:21:05 +0100

    Upgrade kibana and elasticsearch to version 5.0

--- log-analysis/kibana/install.sh
+++
@@ -1,6 +0,0 @@
-KIBANA_BASENAME=kibana-4.6.1-linux-x86_64
-KIBANA_ARCHIVE=$KIBANA_BASENAME'.tar.gz'
-wget -nv https://download.elastic.co/kibana/kibana/$KIBANA_ARCHIVE
-tar xzf $KIBANA_ARCHIVE -C /data
-rm $KIBANA_ARCHIVE
-ln -s /data/$KIBANA_BASENAME /data/kibana
--- log-analysis/kibana/kibana-entrypoint.sh
+++ log-analysis/kibana/kibana-entrypoint.sh
@@ -10,4 +10,4 @@ done
 cd beats-dashboards-1.3.1/
 ./load.sh -url elasticsearch:9200
 
-/data/kibana/bin/kibana
+/docker-entrypoint.sh kibana
