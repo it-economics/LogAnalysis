5bb8324f0b4a9e0e02d8c6b89afc44800c72bd09
roland.voelker@gmx.de <lord_helmchen>
2016-11-30 10:42:32 +0100

    Updated to latest Elastic Stack Version

--- order-service/src/main/docker/install-filebeat.sh
+++ order-service/src/main/docker/install-filebeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L -O https://download.elastic.co/beats/filebeat/filebeat_1.3.1_amd64.deb
-dpkg -i filebeat_1.3.1_amd64.deb
-rm filebeat_1.3.1_amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.1-amd64.deb
+dpkg -i filebeat-5.0.1-amd64.deb
+rm filebeat-5.0.1-amd64.deb
