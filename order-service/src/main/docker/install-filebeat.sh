5ff98c706b7069e8e5808ce66109ddc6dd028d6a
Christian Trampnau <ctrampnau@it-economics.de>
2016-12-14 11:32:08 +0100

    bumped ELK version to 5.1.1

--- order-service/src/main/docker/install-filebeat.sh
+++ order-service/src/main/docker/install-filebeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.1-amd64.deb
-dpkg -i filebeat-5.0.1-amd64.deb
-rm filebeat-5.0.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.1.1-amd64.deb
+dpkg -i filebeat-5.1.1-amd64.deb
+rm filebeat-5.1.1-amd64.deb
