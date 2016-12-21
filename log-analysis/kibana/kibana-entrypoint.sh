2886f1bfc93788729ce1efd7ce02c042a34f51b3
Martin Reiss <martinreiss1@gmail.com>
2016-12-21 14:53:27 +0100

    Automatic dashboard
    Adds automatically configured search, visualization and dashboard
    to Kibana. They are added to Kibana's ES index via curl from its
    entrypoint script.

--- log-analysis/kibana/kibana-entrypoint.sh
+++ log-analysis/kibana/kibana-entrypoint.sh
@@ -9,4 +9,9 @@ while true; do
     nc -q 1 elasticsearch 9200 2>/dev/null && break
 done
 
+echo "Importing search, visualization and dashboard into Kibana"
+curl -XPUT 'http://elasticsearch:9200/.kibana/search/All-Log-Events' -d @/All-Log-Events.json --header "Content-Type: application/json"
+curl -XPUT 'http://elasticsearch:9200/.kibana/visualization/All-Log-Events-per-time' -d @/All-Log-Events-per-time.json --header "Content-Type: application/json"
+curl -XPUT 'http://elasticsearch:9200/.kibana/dashboard/All-Log-Events-Dashboard' -d @/All-Log-Events-Dashboard.json --header "Content-Type: application/json"
+
 /docker-entrypoint.sh kibana
